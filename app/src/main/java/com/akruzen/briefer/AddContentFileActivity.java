4a4e11ad893d44902a45380811c99795349ec500
sherrywilson <sherry@deepsense.in>
2024-11-12 06:16:06 +0000

    fixed file upload

--- app/src/main/java/com/akruzen/briefer/AddContentFileActivity.java
+++ app/src/main/java/com/akruzen/briefer/AddContentFileActivity.java
@@ -4,22 +4,19 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.documentfile.provider.DocumentFile;
-
-import android.app.Activity;
+import androidx.room.Room;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.util.Log;
 import android.content.ContentResolver;
-import android.content.Intent;
-import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.os.Handler;
 import android.os.Looper;
-import android.provider.DocumentsContract;
-import android.provider.MediaStore;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.akruzen.briefer.Constants.Constants;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.card.MaterialCardView;
@@ -28,12 +25,17 @@ import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.parser.PdfTextExtractor;
-
+import com.akruzen.briefer.db.AppDatabase;
+import com.akruzen.briefer.db.Topic;
+import com.akruzen.briefer.db.TopicDao;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Objects;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class AddContentFileActivity extends AppCompatActivity {
 
@@ -49,15 +51,52 @@ public class AddContentFileActivity extends AppCompatActivity {
     ConstraintLayout constraintLayout;
     MaterialButton fileTypeButton;
     MaterialCardView splittingCardView;
+    String extractedContent = "";
+
+    TopicDao topicDao;
+    AppDatabase db;
 
     public void saveFABClicked(View view) {
-        // Save the file
+        // Save the file if content is available
+        if (fileName != null && !extractedContent.isEmpty()) {
+            saveTopicToDatabase(fileName, extractedContent); // Save the topic with file name and content
+//            Toast.makeText(this, "Topic saved", Toast.LENGTH_SHORT).show();
+
+        } else {
+            Toast.makeText(this, "No content to save", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void saveTopicToDatabase(String title, String content) {
+        try {
+            String currDateTime = new SimpleDateFormat("yyyyMMddHHmmss", Locale.UK).format(new Date());
+            int appVersion;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.PackageInfoFlags.of(0));
+                appVersion = (int) pInfo.getLongVersionCode();
+            } else {
+                appVersion = getPackageManager().getPackageInfo(getPackageName(), 0).versionCode;
+            }
+            int sdkVersion = Build.VERSION.SDK_INT;
+
+            Topic topic = new Topic(
+                    title, content, Topic.TYPE_PLAIN_TEXT, currDateTime, appVersion, sdkVersion
+            );
+            topicDao.insertTopic(topic);
+
+            Log.i("Database", "Topic saved with title: " + title);
+            finish(); // Close the activity after saving
+        } catch (PackageManager.NameNotFoundException e) {
+            Toast.makeText(this, "Error saving topic to database", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_content_file);
+
         // Find view by id
         questionFileTextInputEditText = findViewById(R.id.questionFileTextInput);
         fileTypeButton = findViewById(R.id.fileTypeButton);
@@ -68,8 +107,13 @@ public class AddContentFileActivity extends AppCompatActivity {
         splittingTV = findViewById(R.id.splittingTV);
         constraintLayout = findViewById(R.id.addContentFileActivityConstraintLayout);
         splittingCardView = findViewById(R.id.splittingCardView);
-        // Object creation
+
+        // Initialize TinyDB and Room database
+        db = Room.databaseBuilder(this, AppDatabase.class, "TopicDatabase").allowMainThreadQueries().build();
+        topicDao = db.topicDao();
         tinyDB = new TinyDB(this);
+
+
         // Method Calls
         getIntentExtras();
         setTexts();
@@ -91,21 +135,21 @@ public class AddContentFileActivity extends AppCompatActivity {
             executor.shutdownNow(); // Stop the thread when tapped on dismiss
         });
         AlertDialog dialog = builder.create();
-        // Show dialog before starting the test
-        handler.post(dialog::show); // You can replace lambda function with method reference
+        handler.post(dialog::show);
+
         executor.execute(() -> {
-            // Background work goes here
             try {
                 StringBuilder extractedText = new StringBuilder();
                 reader = setPdfReader();
                 int pageCount = reader.getNumberOfPages();
                 for (int i = 0; i < pageCount; i++) {
-                    // to extract the PDF content from the different pages
                     extractedText.append(PdfTextExtractor.getTextFromPage(reader, i + 1).trim()).append("\n");
                 }
-                int charCount = extractedText.toString().trim().length();
+                extractedContent = extractedText.toString().trim();
+                int charCount = extractedContent.length();
                 documentFile = DocumentFile.fromSingleUri(this, selectedFileUri);
                 fileName = documentFile.getName();
+
                 runOnUiThread(() -> {
                     fileTypeButton.setText(fileExtension);
                     pagesTV.setText("Pages: " + pageCount);
@@ -117,9 +161,9 @@ public class AddContentFileActivity extends AppCompatActivity {
                         s.show();
                     }
                     String charLimitStr = tinyDB.getString(Constants.getCharLimitKey());
-                    int charLimit = charLimitStr == "" ? Integer.parseInt(Constants.DEFAULT_CHAR_LIMIT) : Integer.parseInt(charLimitStr);
+                    int charLimit = charLimitStr.isEmpty() ? Integer.parseInt(Constants.DEFAULT_CHAR_LIMIT) : Integer.parseInt(charLimitStr);
                     if (charCount > charLimit) {
-                        splittingTV.setText("Splitting: " + (int)(Math.ceil(charCount / charLimit)) + " parts");
+                        splittingTV.setText("Splitting: " + (int)(Math.ceil((double)charCount / charLimit)) + " parts");
                         splittingCardView.setVisibility(View.VISIBLE);
                     } else {
                         splittingTV.setText("Splitting: No splitting required");
@@ -136,7 +180,6 @@ public class AddContentFileActivity extends AppCompatActivity {
                 runOnUiThread(() -> Toast.makeText(this, "Error reading the PDF", Toast.LENGTH_SHORT).show());
                 throw new RuntimeException(e);
             }
-
         });
     }
 
@@ -147,8 +190,6 @@ public class AddContentFileActivity extends AppCompatActivity {
             if (inputStream != null) {
                 return new PdfReader(inputStream);
             } else {
-                // Handle case where inputStream is null
-                // This can happen if the document couldn't be opened
                 Toast.makeText(this, "Could not open the document to read contents", Toast.LENGTH_SHORT).show();
             }
         } catch (IOException e) {
@@ -157,5 +198,4 @@ public class AddContentFileActivity extends AppCompatActivity {
         }
         return null;
     }
-
 }
--- app/src/main/java/com/akruzen/briefer/db/AppDatabase.java
+++ app/src/main/java/com/akruzen/briefer/db/AppDatabase.java
@@ -3,7 +3,7 @@ package com.akruzen.briefer.db;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {Topic.class}, version = 1)
+@Database(entities = {Topic.class}, version = 1, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract TopicDao topicDao();
 }
